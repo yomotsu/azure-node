9d455c5d649ce7b6957820787534d9eb9b23d979
yomotsu <oyamada@pxgrid.com>
2015-10-03 01:08:42 +0900

    update

--- app.js
+++ app.js
@@ -6,30 +6,32 @@ var port = process.env.PORT || 80;
 var express = require('express');
 var app = express();
 // var server = http.createServer(app);
-var io = require('socket.io');//.listen( port );
+var io = require('socket.io');
 
 // io.set( 'transports', [ 'websocket' ] );
 // io.set( 'origins', '*:*' );
 
-http.createServer(function(req, res) {
+// http.createServer(function(req, res) {
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World4\n');
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World4\n');
 
-}).listen(port);
+// }).listen(port);
 
-// io.listen( port )
 
-// io.on( 'connection', function ( socket ) {
+io.on( 'connection', function ( socket ) {
 
-//   console.log( 'connected' );
+  // console.log( 'connected' );
 
-//   socket.on( 'msg', function( data ) {
-//     io.emit( 'msg', data );
-//   } );
+  socket.on( 'msg', function( data ) {
+    io.emit( 'msg', data );
+  } );
 
-//   socket.on( 'disconnect', function () {
-//     console.log( 'disconnected' );
-//   } );
+  socket.on( 'disconnect', function () {
+    // console.log( 'disconnected' );
+  } );
 
-// });
+});
+
+
+io.listen( port );
