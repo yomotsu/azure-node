dc5969d54cc8e28503b5a63966941896a660bd3a
yomotsu <oyamada@pxgrid.com>
2015-10-03 00:58:43 +0900

    update

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 
 // var express = require('express');
 // var app = express();
 // var server = http.createServer(app);
-var io = require('socket.io');//.listen( port );
+var io = require('socket.io').listen( port );
 
 // io.set( 'transports', [ 'websocket' ] );
 // io.set( 'origins', '*:*' );
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World2\n');
+  res.end('Hello World3\n');
 }).listen(port);
 
 
 
-// io.on( 'connection', function ( socket ) {
+io.on( 'connection', function ( socket ) {
 
-//   console.log( 'connected' );
+  console.log( 'connected' );
 
-//   socket.on( 'msg', function( data ) {
-//     io.emit( 'msg', data );
-//   } );
+  socket.on( 'msg', function( data ) {
+    io.emit( 'msg', data );
+  } );
 
-//   socket.on( 'disconnect', function () {
-//     console.log( 'disconnected' );
-//   } );
+  socket.on( 'disconnect', function () {
+    console.log( 'disconnected' );
+  } );
 
-// });
+});
