010bf8b27df1c0437b55fb2d2fcc67053023d7d5
yomotsu <oyamada@pxgrid.com>
2015-10-03 01:48:08 +0900

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
+var server = require('http').createServer();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
