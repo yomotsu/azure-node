288d8c909f2cae0f9658ca75a3ebcaf2425cbe12
yomotsu <oyamada@pxgrid.com>
2015-10-02 18:05:46 +0900

    io

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
+var io = require( 'socket.io' ).listen( port );
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello World\n'+io);
 }).listen(port);
+
